f91718b633f5bfca8db04ad798a18c5d8a6e62de
Agus Stiawan <agus.stiawan2211@gmail.com>
2021-09-10 21:38:42 +0700

    update

--- docs/api/index.js
+++ docs/api/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const swaggerUi = require("swagger-ui-express");
 const apiSpec = require("./swagger/api-spec.json");
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
