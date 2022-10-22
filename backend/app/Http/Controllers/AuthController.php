f455760a4b907ed9b73d0d5f24bd241b7aafdf96
Agus Stiawan <agus.stiawan2211@gmail.com>
2022-10-22 23:32:29 +0700

    [be - auth] feat: add login & register feature

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Auth\LoginAuthRequest;
+use App\Http\Requests\Auth\RegisterAuthRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(RegisterAuthRequest $request)
+    {
+        $validated = $request->validated();
+
+        $user = new User($validated);
+        $user->password = Hash::make($validated['password']);
+        $user->save();
+
+        return response()->json([
+            'message' => 'User created successfully',
+            'data' => $user,
+        ], 201);
+    }
+
+    public function login(LoginAuthRequest $request)
+    {
+        $validated = $request->validated();
+
+        $user = User::where('email', $validated['email'])->first();
+
+        if (!$user || !Hash::check($validated['password'], $user->password)) {
+            return response()->json([
+                'message' => 'Invalid credentials',
+            ], 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'User logged in successfully',
+            'data' => [
+                'user' => $user,
+                'token' => $token,
+            ],
+        ], 200);
+    }
+}
---
+++ backend/tests/Feature/Controller/AuthControllerTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Tests\Feature\Controller;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_register()
+    {
+        $response = $this->postJson('/api/auth/register', [
+            'name' => 'Test User',
+            'email' => 'testuser@example.test',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $response->assertStatus(201);
+
+        $this->assertDatabaseHas('users', [
+            'name' => 'Test User',
+            'email' => 'testuser@example.test',
+        ]);
+
+        $response->assertJsonPath('data.name', 'Test User');
+        $response->assertJsonPath('data.email', 'testuser@example.test');
+    }
+
+    public function test_register_with_invalid_data()
+    {
+        $response = $this->postJson('/api/auth/register', [
+            'name' => 'Test User',
+            'email' => 'testuser@example.test',
+            'password' => 'password',
+            'password_confirmation' => 'password1',
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonStructure(['errors' => ['password']]);
+    }
+
+    public function test_register_with_existing_email()
+    {
+        User::factory()->create(['email' => 'testuser@example.test']);
+
+        $response = $this->postJson('/api/auth/register', [
+            'name' => 'Test User',
+            'email' => 'testuser@example.test',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonStructure(['errors' => ['email']]);
+    }
+
+    public function test_login()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->postJson('/api/auth/login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+
+        $response->assertStatus(200);
+
+        $response->assertJsonPath('data.user.name', $user->name);
+        $response->assertJsonPath('data.user.email', $user->email);
+
+        $response->assertJsonStructure([
+            'data' => [
+                'token',
+            ],
+        ]);
+    }
+
+    public function test_login_with_invalid_data()
+    {
+        $response = $this->postJson('/api/auth/login', [
+            'email' => 'wrong@example.test',
+            'password' => 'password',
+        ]);
+
+        $response->assertStatus(401);
+        $response->assertJsonStructure(['message']);
+    }
+}
