05f80480ff4a74e4bf1a455daa9e05ece609f41d
Agus Stiawan <agus.stiawan2211@gmail.com>
2022-12-13 22:44:32 +0700

    [fe] lib: add roboto font & material-ui icons library

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -6,6 +6,11 @@
  * contain code that should be seen on all pages. (e.g. navigation bar)
  */
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
